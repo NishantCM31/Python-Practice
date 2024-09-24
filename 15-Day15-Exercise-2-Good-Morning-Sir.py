68da4c978438a99a9c29d7afe60d6a51c57ea047
Nishant <nishantcm31@gmail.com>
2024-09-24 23:18:17 +0530

    15 update

--- 15-Day15-Exercise-2-Good-Morning-Sir.py
+++ 15-Day15-Exercise-2-Good-Morning-Sir.py
@@ -1,9 +1,13 @@
 import time
 timestamp = time.strftime('%H:%M:%S')
 print(timestamp)
-timestamp = time.strftime('%H')
+timestamp = int(time.strftime('%H'))
 print(timestamp)
-timestamp = time.strftime('%M')
-print(timestamp)
-timestamp = time.strftime('%S')
-print(timestamp)
+
+if(timestamp>=0 and timestamp<12 ):
+    print("Good Morning")
+
+elif(timestamp>=12 and timestamp<6):
+    print("Good Afternoon")
+else:
+    print("Good Evening")
